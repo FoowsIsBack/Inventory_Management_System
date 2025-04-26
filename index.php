e09ca99b9eb607373d8097eb90f499b42b215165
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-04-26 23:46:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,46 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Inventory Management</title>
+    <title>Inventory Management System</title>
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="icon/icon.png" type="image/x-icon">
 </head>
+
 <body>
     <div class="title">
-        <h1>INVENTORY MANAGEMENT</h1>
+        <h1>INVENTORY MANAGEMENT SYSTEM</h1>
     </div>
 
-    <form action="add.php" method="post">
-        <input type="text" name="item_name" placeholder="Item Name" required>
-        <input type="number" name="quantity" placeholder="Quantity" required>
-        <input type="number" name="price" placeholder="Price" required>
-        <button type="submit">Add Item</button>
-        <button class="tolist" type="button" onclick="window.location.href='list.php'">Item List</button>
-    </form>
+    <div class="ui">
+        <form action="add.php" method="post">
+            <div class="main">
+                <div class="info">
+                    <label for="user">NAME:</label>
+                    <input type="text" name="user_info" placeholder="Name" required>
+                </div>
+
+                <div class="itemName">
+                    <label for="item_name">ITEM:</label>
+                    <input type="text" name="item_name" placeholder="Item" required>
+                </div>
+
+                <div class="quantity">
+                    <label for="">QUANTITY:</label>
+                    <input type="number" name="quantity" placeholder="Quantity" required>
+                </div>                                                       
+
+                <div class="price">
+                    <label for="">PRICE:</label>
+                    <input type="number" name="price" placeholder="Price" required>
+                </div>
+
+                <div class="clickers">
+                    <button type="submit">Add Item</button>
+                    <button class="tolist" type="button" onclick="window.location.href='list.php'">Item List</button>
+                </div>
+            </div>
+        </form>
+    </div>
 
     <?php include 'inventory_table.php'; ?>
 
