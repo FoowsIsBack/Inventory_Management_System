fb63de9dc49bc37c8c311a07d2174c8118f86519
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-04-28 17:55:18 +0800

    Update add.php

--- add.php
+++ add.php
@@ -4,11 +4,13 @@ ini_set('display_errors', 1);
 
 include 'db.php';
 
-$name = $_POST['item_name'];
+$info = ucwords($_POST['user_info']);
+$gender = ucwords($_POST['gender']);
+$name = ucwords($_POST['item_name']);
 $qty = $_POST['quantity'];
 $price = $_POST['price'];
 
-$conn->query("INSERT INTO inventory (item_name, quantity, price) VALUES ('$name', '$qty', '$price')");
+$conn->query("INSERT INTO inventory (user_info, gender, item_name, quantity, price) VALUES ('$info', '$gender', '$name', '$qty', '$price')");
 
-header("Location: main.php");
+header("Location: index.php");
 ?>
