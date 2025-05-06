329aea452d331756c3ca38cf2973b464b3f77677
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-05-06 19:09:36 +0800

    Update list.php

--- list.php
+++ list.php
@@ -17,12 +17,8 @@
         <table>
             <tr class="classTH">
                 <th>ID</th>
-                <th>NAME</th>
-                <th>PICTURE</th>
-                <th>GENDER</th>
-                <th>ITEM</th>
-                <th>QTY</th>
-                <th>PRICE</th>
+                <th>FULL NAME</th>
+                <th>AGE</th>
                 <th>SETTINGS</th>
             </tr>
 
@@ -30,20 +26,12 @@
             $result = $conn->query("SELECT * FROM inventory");
             while ($row = $result->fetch_assoc()):
             ?>
-            
             <tr>
-                <td><?= $row['id'] ?></td>
-                <td><?= ucwords($row['user_info']) ?></td>
+                <td><?= htmlspecialchars($row['id']) ?></td>
+                <td><?= ucwords(htmlspecialchars($row['user_info'])) ?></td>
+                <td><?= (int)$row['age'] ?></td>
                 <td>
-                    <img src="uploads/<?= htmlspecialchars($row['profile_pic']) ?>" class="pic">
-                </td>
-                <td><?= ucwords($row['gender']) ?></td>
-                <td><?= ucwords($row['item_name']) ?></td>
-                <td><?= $row['quantity'] ?></td>
-                <td>₱ <?= $row['price'] ?></td>
-                <td>
-                    <a class="setting" href="update.php?id=<?= $row['id'] ?>">Edit</a>
-                    <a class="setting" id="del" href="delete.php?id=<?= $row['id'] ?>" onclick="return confirm('Delete this item?')">Delete</a>
+                    <a class="setting" href="view_details.php?id=<?= $row['id'] ?>">View Details</a>
                 </td>
             </tr>
             <?php endwhile; ?>
@@ -52,6 +40,7 @@
 
     <div class="back">
         <button class="backers" onclick="window.location.href='index.php'">Back</button>
+        <button class="backers1" onclick="window.location.href='view_all_details.php'">All Details</button>
     </div>
 
 </body>
