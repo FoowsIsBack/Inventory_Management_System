f9a326a635738341c7f7d5706e80fbbf98c2f886
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-05-06 18:17:41 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="icon/icon.png" type="image/x-icon">
+    <title>Inventory Management System</title>
+</head>
+<body>
+
+    <div class="title1">
+        <h1>INVENTORY MANAGEMENT SYSTEM</h1>
+    </div>
+
+    <div class="logo">
+        <img class="logo_dex" src="icon/logo.png" alt="">
+    </div>
+
+    <div class="choice">
+        <h1>ARE YOU?</h1>
+        <div class="child">
+            <button class="tolist" type="button" onclick="window.location.href='main.php'">STUDENT</button>
+            <p>OR</p>
+            <div class="child1">
+                <button class="admin" type="button" onclick="window.location.href='login.php'">TEACHER</button>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
